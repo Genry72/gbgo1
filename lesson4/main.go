d10872f71edea2e2347ee325d43445a9ce57f680
Valentin Semenov <valentin.semyonov@nexign.com>
2022-08-14 18:04:46 +0300

    add lesson4

---
+++ lesson4/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//1. Познакомьтесь с алгоритмом сортировки вставками. Напишите приложение, которое принимает на вход набор целых
+//чисел и отдаёт на выходе его же в отсортированном виде. Сохраните код, он понадобится нам в дальнейших уроках.
+func main() {
+	var b string
+	fmt.Println("Введите целые числа через запятую")
+	fmt.Scan(&b)
+	var slice []int
+	for _, str := range strings.Split(b, ",") {
+		intt, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Printf("%s не число:%v\n", str, err)
+			return
+		}
+		slice = append(slice, intt)
+	}
+	sortSlice(slice)
+	fmt.Printf("Ваша последовательнотсть чисел поссле сортировки: %v\n", slice)
+}
+
+func sortSlice(array []int) {
+	for i := 1; i < len(array); i++ {
+		j := i
+		for j > 0 {
+			if array[j-1] > array[j] {
+				array[j-1], array[j] = array[j], array[j-1]
+			}
+			j = j - 1
+		}
+	}
+}
